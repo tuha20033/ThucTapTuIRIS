5d55e7ec04bb462b35b22833eb597f1cd32788aa
tuha20033 <143210753+tuha20033@users.noreply.github.com>
2026-01-05 00:17:22 +0700

    Update IAuditable.cs

--- WebPortal.Domain/Common/IAuditable.cs
+++ WebPortal.Domain/Common/IAuditable.cs
@@ -2,9 +2,7 @@ using System;
 
 namespace WebPortal.Domain.Common;
 
-/// <summary>
-/// Marker for entities with CreatedAt/UpdatedAt timestamps.
-/// </summary>
+
 public interface IAuditable
 {
     DateTime CreatedAt { get; set; }
